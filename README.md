ee874c9fd1c76ca74b24db4ae7c78661b5be1c47
Nikhil Marathe <nsm.nikhil@gmail.com>
2015-08-28 23:56:49 -0700

    Initial commit. Works, but no error handling

---
+++ README.md
@@ -0,0 +1,20 @@
+
+config.ini:
+
+    api_key = ""
+    user_id = ""
+    size = "Medium 640"
+
+
+run:
+
+    node bin.js
+
+prints out all albums (photosets)
+
+    node bin.js photosetid
+
+generates flickr shortcode for each photo in the photoset.
+
+Copy over flickr.html to layouts/shortcodes/ in your Hugo installation and you
+are ready to go!
---
+++ bin.js
@@ -0,0 +1,49 @@
+var ini = require('ini')
+  , fs = require('fs')
+  , flickrapi = require('flickrapi')
+
+var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+flickrapi.tokenOnly({ api_key: config.api_key }, function(err, flickr) {
+  if (process.argv.length < 3) {
+    flickr.photosets.getList({ user_id: config.user_id }, function(err, response) {
+      response.photosets.photoset.forEach(function(photoset) {
+        console.log(photoset.id, photoset.title._content);
+      });
+      process.exit(0);
+    });
+  } else {
+    var photosetId = process.argv[2];
+    flickr.photosets.getPhotos({ user_id: config.user_id, photoset_id: photosetId }, function(err, response) {
+      counter = response.photoset.photo.length;
+      response.photoset.photo.forEach(function(photo) {
+        dumpPhotoInfo(photo, function() {
+          counter--;
+          if (!counter) {
+            process.exit(0);
+          }
+        });
+      });
+    })
+  }
+
+  function printHugoCaption(title, caption, link, source) {
+    var sep = '"\n           "';
+    var embed = '{{% flickr "' + title + sep + caption + sep + link + sep + source + '" %}}';
+    console.log(embed);
+  }
+
+  function dumpPhotoInfo(photo, cb) {
+    flickr.photos.getInfo({ photo_id: photo.id }, function(err, response) {
+      var photo_info = response.photo;
+      flickr.photos.getSizes({ photo_id: photo.id }, function(err, response) {
+        response.sizes.size.forEach(function(size) {
+          if (size.label.toLowerCase() === config.size.toLowerCase()) {
+            printHugoCaption(photo_info.title._content, photo_info.description._content, photo_info.urls.url[0]._content, size.source);
+          }
+        });
+        cb();
+      });
+    });
+  }
+});
